e1aeb630a93106ed9aea8235dbee9fd79215dca1
Francisco Juan <juansoussev@gmail.com>
2023-11-23 00:34:47 -0300

    connection with db.json

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -4,13 +4,22 @@ export default function Home() {
     const [name, setName] = useState("")
 
     useEffect(() => {
-        fetch('http://localhost:3000/teste')
+        fetch('http://localhost:3000/teste/1')
           .then(response => response.json())
           .then(data => setName(data.name))
     }, []) 
 
+    function send() {
+        fetch('http://localhost:3000/teste', { age: 2, id: 3, name: name })
+    }
+
     return (
-        <h1>{name}</h1>
-        form
+        <>
+            <h1>{name}</h1>
+            <form action="POST">
+                <input type="text" value={name} />
+                <button onSubmit={send} type="submit">envia p pai</button>
+            </form>
+        </>
     )
 }
